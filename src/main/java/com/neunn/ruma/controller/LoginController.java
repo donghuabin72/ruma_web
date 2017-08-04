9533d66dfe414140c743c8cbb91a42f54bc434d0
董华斌 <donghb@neunn.com>
2017-08-04 11:46:10 +0800

    啦啦啦

--- src/main/java/com/neunn/ruma/controller/LoginController.java
+++ src/main/java/com/neunn/ruma/controller/LoginController.java
@@ -154,9 +154,7 @@ public class LoginController extends BaseController {
     public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
         return "modules/sys/login";
     }
-    
-
-    
+  
     @RequestMapping(value = "/checkValidateCode")
     public void checkValidateCode(HttpServletRequest request, HttpServletResponse response, Model model) throws IOException {
         String validateCode = request.getParameter(VALIDATE_CODE);
